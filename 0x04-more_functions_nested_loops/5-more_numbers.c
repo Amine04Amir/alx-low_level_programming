7838ed6ca2c0b977b71954dc2dc9e90d97d23ccf
Amine04Amir <raja_amir@outlook.fr>
2023-03-23 16:40:18 -0700

    ad

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+   *more_numbers - prints numbers from 0 to 14
+   *
+   * Return: 0 success
+   */
+
+void more_numbers(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 10; n1++)
+	{
+		for (n2 = 0; <= 14; n2++)
+		{
+			if (n2 > 9)
+			{
+				putchar((n2 / 10) + '0');
+			}
+			putchar((n2 % 10) + '0');
+		}
+		putchar(10);
+	}
+}
